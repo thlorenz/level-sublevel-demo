2c4e23ae2d9da4405354f6c3e8607cb7e118637e
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-22 11:33:22 -0400

    storing countries

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 'use strict';
 
-var store = module.exports = function (db, countries, cb) {
+function puts(entries) {
+  return Object.keys(entries)
+    .map(function (k) {
+      return { type: 'put', key: k, value: entries[k] };
+    });
+}
 
+var store = module.exports = function (db, entries, cb) {
+  var countries = db.sublevel('countries', { valueEncoding: 'json' });
+
+
+  countries.batch(puts(entries), function (err) {
+    cb(err, { countries: countries })  
+  });
 };
 
 
-
-
-
-if (module.parent) return;
-
 var level    =  require('level-test')( { mem: true })
   , sublevel =  require('level-sublevel')
+  , dump     =  require('level-dump')
   , db       =  sublevel(level(null, { valueEncoding: 'json' }));
 
 var countries = {
@@ -20,3 +28,11 @@ var countries = {
   ,  Germany:   { language: 'German'  ,  capital: 'Berlin'        }
   ,  Austria:   { language: 'German'  ,  capital: 'Vienna'        }
 };
+
+store(db, countries, function (err, sublevels) {
+  if (err) return console.error(err);
+  console.log('\n=== dump ===')
+  // dump(db);                // dumps nothing since countries are separated from root db
+  // dump.allEntries(db)      // dumps entire db including sublevels and shows how keys are namespaced
+  dump(sublevels.countries);
+});
