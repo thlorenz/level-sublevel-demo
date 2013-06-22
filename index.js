4fa2e4c60356d2851e918fa3732fa7632fe0e5a3
Thorsten Lorenz <thlorenz@gmx.de>
2013-06-22 12:32:30 -0400

    indexing by capital

--- index.js
+++ index.js
@@ -9,21 +9,20 @@ function puts(entries) {
 
 var store = module.exports = function (db, entries, cb) {
   var countries = db.sublevel('countries', { valueEncoding: 'json' })
-    , byLanguage =  db.sublevel('byLanguage', { valueEncoding: 'utf8'})
+    , byCapital  =  db.sublevel('byCapital', { valueEncoding: 'utf8' });
 
   countries.pre(function (val, add) {
-    add({ prefix :  byLanguage
+    add({ prefix :  byCapital
         , type   :  'put'
-        , key    :  val.value.language
+        , key    :  val.value.capital
         , value  :  val.key
     });
   });
 
   countries.batch(puts(entries), function (err) {
-    cb(err, { countries: countries, byLanguage: byLanguage })  
+    cb(err, { countries: countries, byCapital: byCapital })  
   });
 };
-
 
 var level    =  require('level-test')( { mem: true })
   , sublevel =  require('level-sublevel')
@@ -37,11 +36,20 @@ var countries = {
   ,  Austria:   { language: 'German'  ,  capital: 'Vienna'        }
 };
 
+function whoseCapitalIs(capital, sublevels) {
+  sublevels.byCapital.get(capital, function (err, country) {
+    if (err) return console.error(err);
+    console.log('%s is the capital of %s.', capital, country);
+  });
+}
+
 store(db, countries, function (err, sublevels) {
   if (err) return console.error(err);
   console.log('\n=== dump ===')
   // dump(db);                // dumps nothing since countries are separated from root db
   // dump.allEntries(db)      // dumps entire db including sublevels and shows how keys are namespaced
   // dump(sublevels.countries);
-  dump(sublevels.byLanguage);
+  dump(sublevels.byCapital);
+  // whoseCapitalIs('Berlin', sublevels);
 });
+
